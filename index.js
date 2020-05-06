ab5d407b3fd05977d0c74d95c179a51a4cd19f99
zeyus <zeyus@zeyus.com>
2020-05-06 14:03:18 +0200

    Added telephone contacts, added labels for insta and linkedin.

--- index.js
+++ index.js
@@ -1,59 +1,89 @@
+'use strict';
+
 const fs = require('fs');
 const https = require('https');
-const xml2js       = require('xml2js');
-const parser       = new xml2js.Parser();
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
 
 const data_path = './data/';
 const source_base_uri = 'https://www.europarl.europa.eu/meps/en/';
-const master_source = ['full-list/xml/', 'member-list.xml'];
 const combined_source = 'members_by_country.json';
-// var argv = require('minimist')(process.argv.slice(2));
-const download = false;
-const create_combined_source = true;
-var alphasources = [];
-var sources = [];
+let argv = require('minimist')(process.argv.slice(2));
 
-for(let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+const download = !argv.hasOwnProperty('download') || argv.download != false;
+const combine = !argv.hasOwnProperty('combine') || argv.combine != false;
+
+// How long to wait in miliseconds beween downloads.
+const wait = argv.hasOwnProperty('combine') ? argv.hasOwnProperty('combine') : 300;
+
+var alphasources = [];
+
+for (let i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
     alphasources.push(['odp/detailed-list-xml/' + String.fromCharCode(i), String.fromCharCode(i) + '.xml']);
 }
 
-sources.push(master_source);
-sources.push(...alphasources);
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
-
-const master_file = './data/member-list.xml';
-
-// console.log(fs.readFileSync('./data/a.xml', {encoding: 'utf-8'}));
-
-
-// https://www.europarl.europa.eu/meps/en/full-list/xml/
-
-// https://www.europarl.europa.eu/meps/en/odp/detailed-list-xml/c
+// Grab the remote data source and save a local copy.
 function download_data_file(remote, local) {
-    var dest = fs.createWriteStream(data_path + local);
-    const request = https.get(source_base_uri + remote, function(response) {
-      response.pipe(dest);
+    return new Promise((resolve, reject) => {
+        var dest = fs.createWriteStream(data_path + local);
+        https.get(source_base_uri + remote, function (response) {
+            response.pipe(dest);
+            response.on('end', async function () {
+                if (!dest.writableFinished) {
+                    await new Promise(fulfill => dest.on("finish", () => {
+                        ;
+                        dest.close();
+                        fulfill();
+                        resolve(true);
+                    }));
+                } else {
+                    dest.close();
+                    resolve(true);
+                }
+            });
+        }).on('error', (e) => {
+            dest.close();
+            console.log(e.message);
+            reject(e);
+        });
     });
 }
 
-if(download) {
-    // Download
-    sources.forEach(function(source){
-        console.log('Downloading data file... ' + source[0] + ' to: ' + source[1])
-        download_data_file(source[0], source[1]);
+async function download_data_files() {
+    return new Promise(async (resolve, reject) => {
+        let completed = 0;
+        for (let source of alphasources) {
+            console.log('Saving source ' + source[0] + ' to: ' + data_path + source[1])
+            await download_data_file(source[0], source[1]);
+            await sleep(wait);
+            completed++;
+        }
+        if (completed == alphasources.length) {
+            resolve(true);
+        } else {
+            reject(false);
+        }
     });
 }
 
-if (create_combined_source) {
+// Combine alphabetic contact lists into single JSON object.
+function combine_data_files() {
+    console.log('Combining data sources');
     var members_by_country = {};
-    alphasources.forEach(function(source){
+    alphasources.forEach(function (source) {
         let xml_string = fs.readFileSync(data_path + source[1], "utf8");
-        parser.parseString(xml_string, function(error, result) {
-            if(error === null) {
+        parser.parseString(xml_string, function (error, result) {
+            if (error === null) {
                 if (result.meps.mep) {
-                    result.meps.mep.forEach(function(member){
-                        var country = member.country[0]._;
-                        if (!members_by_country[country]){
+                    result.meps.mep.forEach(function (member) {
+                        let country = member.country[0]._;
+                        if (!members_by_country[country]) {
                             members_by_country[country] = [];
                         }
                         members_by_country[country].push(member);
@@ -65,8 +95,28 @@ if (create_combined_source) {
             }
         });
     });
-    var dest = fs.createWriteStream(data_path + combined_source);
+    console.log('Saving data to ' + data_path + combined_source)
+    let dest = fs.createWriteStream(data_path + combined_source);
     dest.write(JSON.stringify(members_by_country));
     dest.close();
 }
 
+
+async function main() {
+    if (download) {
+        console.log('Downloading data files.');
+        await download_data_files();
+        console.log('All downloads complete');
+        if (combine) {
+
+            combine_data_files();
+        }
+    }
+    else if (combine) {
+        console.log('Combining data sources');
+        combine_data_files();
+    }
+    console.log('All done!');
+}
+
+main();
